0b9b797b5466fa65f3f2b8b59ac55c59a719f564
Pyotr <2chilavert3@mail.ru>
2020-09-03 23:30:11 +0300

    Закончена вёрстка страницы about.

---
+++ src/js/constants/swiperConfig.js
@@ -0,0 +1,23 @@
+export const SWIPER_CONFIG = {
+  pagination: {
+    el: '.swiper__pagination',
+    clickable: true,
+    bulletClass: 'swiper__bullet',
+    bulletActiveClass: 'swiper__bullet_active'
+  },
+  navigation: {
+    nextEl: '.swiper__button-next',
+    prevEl: '.swiper__button-prev',
+  },
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  grabCursor: true,
+  // slidesPerView: 4,
+  // spaceBetween: 30,
+  // direction: 'horizontal',
+  // centeredSlides: true,
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   clickable: true,
+  // },
+}
--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,1 +1,11 @@
 import './about.css';
+import Swiper, { Navigation, Pagination } from 'swiper';
+import { SWIPER_CONFIG } from '../../js/constants/swiperConfig';
+
+Swiper.use([Navigation, Pagination]);
+
+function initSwiper() {
+  new Swiper('.swiper__container', SWIPER_CONFIG);
+}
+
+initSwiper();
